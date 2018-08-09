ed246b894ed98753c8f0ff058cd57fb762078ad3
isson <dlstjr53@gmail.com>
2018-08-09 16:31:21 +0900

    Window에서 Docker image build 시 줄의 끝문자 Unix 형식으로 변환

--- build.sh
+++ build.sh
@@ -8,6 +8,28 @@ cd $DIR
 
 trap '[ "$?" -eq 0 ] || read -p "Looks like something went wrong... Press any key to continue..."' EXIT
 
+
+# for windows with docker for Windows
+# =====================================================================================================================
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "Linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "MAC OSX"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "Windows-Cygwin"
+	dos2unix.exe ./.bash_profile ./.docker-entrypoint.sh ./6379-docker.conf ./supervisord.conf
+elif [[ "$OSTYPE" == "msys" ]]; then
+    echo "Windows-Msys"
+	dos2unix.exe ./.bash_profile ./.docker-entrypoint.sh ./6379-docker.conf ./supervisord.conf
+elif [[ "$OSTYPE" == "win32" ]]; then
+    echo "Windows-Win32"
+	dos2unix.exe ./.bash_profile ./.docker-entrypoint.sh ./6379-docker.conf ./supervisord.conf
+else
+    echo "Unknown"
+	exit
+fi
+
 read -p "tag (default \"latest\"): " TAG
 if [ -z "$TAG" ]; then
   TAG=latest
